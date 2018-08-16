b2192c751c65a55d7e1dec480747b7129f4205f9
jealno <sii_jere@hotmail.com>
2018-08-16 20:55:07 +0700

    Initial commit

---
+++ BypassInetPositif.bat
@@ -0,0 +1,218 @@
+@echo off
+
+SET NEWLINE=^& echo.
+
+FIND /C /I "119.81.2.142" %WINDIR%\system32\drivers\etc\hosts
+IF %ERRORLEVEL% NEQ 0 ECHO %NEWLINE%^119.81.2.142 kbagi.com www.kbagi.com>>%WINDIR%\System32\drivers\etc\hosts
+
+FIND /C /I "i.kbagi.com" %WINDIR%\system32\drivers\etc\hosts
+IF %ERRORLEVEL% NEQ 0 ECHO %NEWLINE%^119.81.2.139 i.kbagi.com>>%WINDIR%\System32\drivers\etc\hosts
+
+FIND /C /I "s1.kbagi.com" %WINDIR%\system32\drivers\etc\hosts
+IF %ERRORLEVEL% NEQ 0 ECHO %NEWLINE%^119.81.2.141 s1.kbagi.com>>%WINDIR%\System32\drivers\etc\hosts
+
+FIND /C /I "s2.kbagi.com" %WINDIR%\system32\drivers\etc\hosts
+IF %ERRORLEVEL% NEQ 0 ECHO %NEWLINE%^119.81.2.139 s2.kbagi.com>>%WINDIR%\System32\drivers\etc\hosts
+
+FIND /C /I "s3.kbagi.com" %WINDIR%\system32\drivers\etc\hosts
+IF %ERRORLEVEL% NEQ 0 ECHO %NEWLINE%^119.81.2.140 s3.kbagi.com>>%WINDIR%\System32\drivers\etc\hosts
+
+FIND /C /I "s200.kbagi.com" %WINDIR%\system32\drivers\etc\hosts
+IF %ERRORLEVEL% NEQ 0 ECHO %NEWLINE%^209.58.183.2 s200.kbagi.com>>%WINDIR%\System32\drivers\etc\hosts
+
+FIND /C /I "s201.kbagi.com" %WINDIR%\system32\drivers\etc\hosts
+IF %ERRORLEVEL% NEQ 0 ECHO %NEWLINE%^209.58.183.3 s201.kbagi.com>>%WINDIR%\System32\drivers\etc\hosts
+
+FIND /C /I "s202.kbagi.com" %WINDIR%\system32\drivers\etc\hosts
+IF %ERRORLEVEL% NEQ 0 ECHO %NEWLINE%^209.58.183.4 s202.kbagi.com>>%WINDIR%\System32\drivers\etc\hosts
+
+FIND /C /I "s203.kbagi.com" %WINDIR%\system32\drivers\etc\hosts
+IF %ERRORLEVEL% NEQ 0 ECHO %NEWLINE%^209.58.183.5 s203.kbagi.com>>%WINDIR%\System32\drivers\etc\hosts
+
+FIND /C /I "s204.kbagi.com" %WINDIR%\system32\drivers\etc\hosts
+IF %ERRORLEVEL% NEQ 0 ECHO %NEWLINE%^209.58.183.6 s204.kbagi.com>>%WINDIR%\System32\drivers\etc\hosts
+
+FIND /C /I "s205.kbagi.com" %WINDIR%\system32\drivers\etc\hosts
+IF %ERRORLEVEL% NEQ 0 ECHO %NEWLINE%^209.58.183.7 s205.kbagi.com>>%WINDIR%\System32\drivers\etc\hosts
+
+FIND /C /I "s206.kbagi.com" %WINDIR%\system32\drivers\etc\hosts
+IF %ERRORLEVEL% NEQ 0 ECHO %NEWLINE%^209.58.183.8 s206.kbagi.com>>%WINDIR%\System32\drivers\etc\hosts
+
+FIND /C /I "s207.kbagi.com" %WINDIR%\system32\drivers\etc\hosts
+IF %ERRORLEVEL% NEQ 0 ECHO %NEWLINE%^209.58.183.9 s207.kbagi.com>>%WINDIR%\System32\drivers\etc\hosts
+
+FIND /C /I "s208.kbagi.com" %WINDIR%\system32\drivers\etc\hosts
+IF %ERRORLEVEL% NEQ 0 ECHO %NEWLINE%^209.58.183.10 s208.kbagi.com>>%WINDIR%\System32\drivers\etc\hosts
+
+FIND /C /I "s209.kbagi.com" %WINDIR%\system32\drivers\etc\hosts
+IF %ERRORLEVEL% NEQ 0 ECHO %NEWLINE%^209.58.183.1 s209.kbagi.com>>%WINDIR%\System32\drivers\etc\hosts
+
+FIND /C /I "s210.kbagi.com" %WINDIR%\system32\drivers\etc\hosts
+IF %ERRORLEVEL% NEQ 0 ECHO %NEWLINE%^209.58.183.12 s210.kbagi.com>>%WINDIR%\System32\drivers\etc\hosts
+
+FIND /C /I "s211.kbagi.com" %WINDIR%\system32\drivers\etc\hosts
+IF %ERRORLEVEL% NEQ 0 ECHO %NEWLINE%^209.58.183.13 s211.kbagi.com>>%WINDIR%\System32\drivers\etc\hosts
+
+FIND /C /I "s212.kbagi.com" %WINDIR%\system32\drivers\etc\hosts
+IF %ERRORLEVEL% NEQ 0 ECHO %NEWLINE%^209.58.183.14 s212.kbagi.com>>%WINDIR%\System32\drivers\etc\hosts
+
+FIND /C /I "s213.kbagi.com" %WINDIR%\system32\drivers\etc\hosts
+IF %ERRORLEVEL% NEQ 0 ECHO %NEWLINE%^209.58.183.15 s213.kbagi.com>>%WINDIR%\System32\drivers\etc\hosts
+
+FIND /C /I "s214.kbagi.com" %WINDIR%\system32\drivers\etc\hosts
+IF %ERRORLEVEL% NEQ 0 ECHO %NEWLINE%^209.58.183.16 s214.kbagi.com>>%WINDIR%\System32\drivers\etc\hosts
+
+FIND /C /I "s215.kbagi.com" %WINDIR%\system32\drivers\etc\hosts
+IF %ERRORLEVEL% NEQ 0 ECHO %NEWLINE%^209.58.183.17 s215.kbagi.com>>%WINDIR%\System32\drivers\etc\hosts
+
+FIND /C /I "s216.kbagi.com" %WINDIR%\system32\drivers\etc\hosts
+IF %ERRORLEVEL% NEQ 0 ECHO %NEWLINE%^209.58.183.18 s216.kbagi.com>>%WINDIR%\System32\drivers\etc\hosts
+
+FIND /C /I "s217.kbagi.com" %WINDIR%\system32\drivers\etc\hosts
+IF %ERRORLEVEL% NEQ 0 ECHO %NEWLINE%^209.58.183.19 s217.kbagi.com>>%WINDIR%\System32\drivers\etc\hosts
+
+FIND /C /I "s218.kbagi.com" %WINDIR%\system32\drivers\etc\hosts
+IF %ERRORLEVEL% NEQ 0 ECHO %NEWLINE%^209.58.183.20 s218.kbagi.com>>%WINDIR%\System32\drivers\etc\hosts
+
+FIND /C /I "s219.kbagi.com" %WINDIR%\system32\drivers\etc\hosts
+IF %ERRORLEVEL% NEQ 0 ECHO %NEWLINE%^209.58.183.21 s219.kbagi.com>>%WINDIR%\System32\drivers\etc\hosts
+
+FIND /C /I "reddit.com" %WINDIR%\system32\drivers\etc\hosts
+IF %ERRORLEVEL% NEQ 0 ECHO %NEWLINE%^151.101.129.140 reddit.com www.reddit.com>>%WINDIR%\System32\drivers\etc\hosts
+
+FIND /C /I "m.reddit.com" %WINDIR%\system32\drivers\etc\hosts
+IF %ERRORLEVEL% NEQ 0 ECHO %NEWLINE%^151.101.85.140	m.reddit.com>>%WINDIR%\System32\drivers\etc\hosts
+
+FIND /C /I "b.thumbs.redditmedia.com" %WINDIR%\system32\drivers\etc\hosts
+IF %ERRORLEVEL% NEQ 0 ECHO %NEWLINE%^151.101.49.140 b.thumbs.redditmedia.com>>%WINDIR%\System32\drivers\etc\hosts
+
+FIND /C /I "i.reddituploads.com" %WINDIR%\system32\drivers\etc\hosts
+IF %ERRORLEVEL% NEQ 0 ECHO %NEWLINE%^151.101.49.140	i.reddituploads.com>>%WINDIR%\System32\drivers\etc\hosts
+
+FIND /C /I "i.redditmedia.com" %WINDIR%\system32\drivers\etc\hosts
+IF %ERRORLEVEL% NEQ 0 ECHO %NEWLINE%^151.101.49.140 i.redditmedia.com>>%WINDIR%\System32\drivers\etc\hosts
+
+FIND /C /I "g.redditmedia.com" %WINDIR%\system32\drivers\etc\hosts
+IF %ERRORLEVEL% NEQ 0 ECHO %NEWLINE%^151.101.49.140 g.redditmedia.com>>%WINDIR%\System32\drivers\etc\hosts
+
+FIND /C /I "a.thumbs.redditmedia.com" %WINDIR%\system32\drivers\etc\hosts
+IF %ERRORLEVEL% NEQ 0 ECHO %NEWLINE%^151.101.49.140	a.thumbs.redditmedia.com>>%WINDIR%\System32\drivers\etc\hosts
+
+FIND /C /I "redditgifts.com" %WINDIR%\system32\drivers\etc\hosts
+IF %ERRORLEVEL% NEQ 0 ECHO %NEWLINE%^151.101.193.140 redditgifts.com>>%WINDIR%\System32\drivers\etc\hosts
+
+FIND /C /I "ssl.reddit.com" %WINDIR%\system32\drivers\etc\hosts
+IF %ERRORLEVEL% NEQ 0 ECHO %NEWLINE%^151.101.129.140 ssl.reddit.com>>%WINDIR%\System32\drivers\etc\hosts
+
+FIND /C /I "blog.reddit.com" %WINDIR%\system32\drivers\etc\hosts
+IF %ERRORLEVEL% NEQ 0 ECHO %NEWLINE%^151.101.129.140 blog.reddit.com>>%WINDIR%\System32\drivers\etc\hosts
+
+FIND /C /I "forum.reddit.com" %WINDIR%\system32\drivers\etc\hosts
+IF %ERRORLEVEL% NEQ 0 ECHO %NEWLINE%^151.101.129.140 forum.reddit.com>>%WINDIR%\System32\drivers\etc\hosts
+
+FIND /C /I "help.reddit.com" %WINDIR%\system32\drivers\etc\hosts
+IF %ERRORLEVEL% NEQ 0 ECHO %NEWLINE%^151.101.129.140 help.reddit.com>>%WINDIR%\System32\drivers\etc\hosts
+
+FIND /C /I "test.reddit.com" %WINDIR%\system32\drivers\etc\hosts
+IF %ERRORLEVEL% NEQ 0 ECHO %NEWLINE%^151.101.129.140 test.reddit.com>>%WINDIR%\System32\drivers\etc\hosts			
+
+FIND /C /I "pay.reddit.com" %WINDIR%\system32\drivers\etc\hosts
+IF %ERRORLEVEL% NEQ 0 ECHO %NEWLINE%^151.101.129.140 pay.reddit.com>>%WINDIR%\System32\drivers\etc\hosts
+
+FIND /C /I "en.reddit.com" %WINDIR%\system32\drivers\etc\hosts
+IF %ERRORLEVEL% NEQ 0 ECHO %NEWLINE%^151.101.129.140 en.reddit.com>>%WINDIR%\System32\drivers\etc\hosts
+
+FIND /C /I "en-us.reddit.com" %WINDIR%\system32\drivers\etc\hosts
+IF %ERRORLEVEL% NEQ 0 ECHO %NEWLINE%^151.101.129.140 en-us.reddit.com>>%WINDIR%\System32\drivers\etc\hosts
+
+FIND /C /I "4x.reddit.com" %WINDIR%\system32\drivers\etc\hosts
+IF %ERRORLEVEL% NEQ 0 ECHO %NEWLINE%^151.101.129.140 4x.reddit.com>>%WINDIR%\System32\drivers\etc\hosts
+
+FIND /C /I "np.reddit.com" %WINDIR%\system32\drivers\etc\hosts
+IF %ERRORLEVEL% NEQ 0 ECHO %NEWLINE%^151.101.129.140 np.reddit.com>>%WINDIR%\System32\drivers\etc\hosts
+
+FIND /C /I "nsfw.reddit.com" %WINDIR%\system32\drivers\etc\hosts
+IF %ERRORLEVEL% NEQ 0 ECHO %NEWLINE%^151.101.129.140 nsfw.reddit.com>>%WINDIR%\System32\drivers\etc\hosts
+
+FIND /C /I "oauth.reddit.com" %WINDIR%\system32\drivers\etc\hosts
+IF %ERRORLEVEL% NEQ 0 ECHO %NEWLINE%^151.101.129.140 oauth.reddit.com>>%WINDIR%\System32\drivers\etc\hosts
+
+FIND /C /I "events.redditmedia.com" %WINDIR%\system32\drivers\etc\hosts
+IF %ERRORLEVEL% NEQ 0 ECHO %NEWLINE%^52.88.110.117 events.redditmedia.com>>%WINDIR%\System32\drivers\etc\hosts
+
+FIND /C /I "www.redditstatic.com" %WINDIR%\system32\drivers\etc\hosts
+IF %ERRORLEVEL% NEQ 0 ECHO %NEWLINE%^151.101.49.140 www.redditstatic.com>>%WINDIR%\System32\drivers\etc\hosts
+
+FIND /C /I "stats.redditmedia.com" %WINDIR%\system32\drivers\etc\hosts
+IF %ERRORLEVEL% NEQ 0 ECHO %NEWLINE%^52.205.201.248 stats.redditmedia.com>>%WINDIR%\System32\drivers\etc\hosts
+
+FIND /C /I "out.reddit.com" %WINDIR%\system32\drivers\etc\hosts
+IF %ERRORLEVEL% NEQ 0 ECHO %NEWLINE%^54.174.14.76 out.reddit.com>>%WINDIR%\System32\drivers\etc\hosts
+
+FIND /C /I "redd.it" %WINDIR%\system32\drivers\etc\hosts
+IF %ERRORLEVEL% NEQ 0 ECHO %NEWLINE%^54.172.97.229 redd.it>>%WINDIR%\System32\drivers\etc\hosts
+
+FIND /C /I "i.redd.it" %WINDIR%\system32\drivers\etc\hosts
+IF %ERRORLEVEL% NEQ 0 ECHO %NEWLINE%^151.101.85.140 i.redd.it>>%WINDIR%\System32\drivers\etc\hosts
+
+FIND /C /I "pixel.redditmedia.com" %WINDIR%\system32\drivers\etc\hosts
+IF %ERRORLEVEL% NEQ 0 ECHO %NEWLINE%^54.173.170.4 pixel.redditmedia.com>>%WINDIR%\System32\drivers\etc\hosts
+
+FIND /C /I "www.redditmedia.com" %WINDIR%\system32\drivers\etc\hosts
+IF %ERRORLEVEL% NEQ 0 ECHO %NEWLINE%^151.101.49.140 www.redditmedia.com>>%WINDIR%\System32\drivers\etc\hosts
+
+FIND /C /I "about.reddit.com" %WINDIR%\system32\drivers\etc\hosts
+IF %ERRORLEVEL% NEQ 0 ECHO %NEWLINE%^151.101.49.140 about.reddit.com>>%WINDIR%\System32\drivers\etc\hosts
+
+FIND /C /I "i.vimeocdn.com" %WINDIR%\system32\drivers\etc\hosts
+IF %ERRORLEVEL% NEQ 0 ECHO %NEWLINE%^151.101.50.109 i.vimeocdn.com>>%WINDIR%\System32\drivers\etc\hosts
+
+FIND /C /I "developer.vimeo.com" %WINDIR%\system32\drivers\etc\hosts
+IF %ERRORLEVEL% NEQ 0 ECHO %NEWLINE%^151.101.192.217 developer.vimeo.com>>%WINDIR%\System32\drivers\etc\hosts
+
+FIND /C /I "www.vimeo.com" %WINDIR%\system32\drivers\etc\hosts
+IF %ERRORLEVEL% NEQ 0 ECHO %NEWLINE%^151.101.192.217 www.vimeo.com vimeo.com>>%WINDIR%\System32\drivers\etc\hosts
+
+FIND /C /I "f.vimeocdn.com" %WINDIR%\system32\drivers\etc\hosts
+IF %ERRORLEVEL% NEQ 0 ECHO %NEWLINE%^151.101.50.109	f.vimeocdn.com>>%WINDIR%\System32\drivers\etc\hosts
+
+FIND /C /I "secure-c.vimeocdn.com" %WINDIR%\system32\drivers\etc\hosts
+IF %ERRORLEVEL% NEQ 0 ECHO %NEWLINE%^23.206.216.24 secure-c.vimeocdn.com>>%WINDIR%\System32\drivers\etc\hosts
+
+FIND /C /I "player.vimeo.com" %WINDIR%\system32\drivers\etc\hosts
+IF %ERRORLEVEL% NEQ 0 ECHO %NEWLINE%^151.101.0.217 player.vimeo.com>>%WINDIR%\System32\drivers\etc\hosts
+
+FIND /C /I "104.27.214.28" %WINDIR%\system32\drivers\etc\hosts
+IF %ERRORLEVEL% NEQ 0 ECHO %NEWLINE%^104.27.214.28 yts.am www.yts.am>>%WINDIR%\System32\drivers\etc\hosts
+
+FIND /C /I "104.16.168.50" %WINDIR%\system32\drivers\etc\hosts
+IF %ERRORLEVEL% NEQ 0 ECHO %NEWLINE%^104.16.168.50 openload.co www.openload.co api.openload.co>>%WINDIR%\System32\drivers\etc\hosts
+
+FIND /C /I "tt1.openload.co" %WINDIR%\system32\drivers\etc\hosts
+IF %ERRORLEVEL% NEQ 0 ECHO %NEWLINE%^104.16.167.50 tt1.openload.co>>%WINDIR%\System32\drivers\etc\hosts
+
+FIND /C /I "t1.openload.co" %WINDIR%\system32\drivers\etc\hosts
+IF %ERRORLEVEL% NEQ 0 ECHO %NEWLINE%^104.16.166.50 t1.openload.co>>%WINDIR%\System32\drivers\etc\hosts
+
+FIND /C /I "support.openload.co" %WINDIR%\system32\drivers\etc\hosts
+IF %ERRORLEVEL% NEQ 0 ECHO %NEWLINE%^104.16.164.50 support.openload.co>>%WINDIR%\System32\drivers\etc\hosts
+cls
+ECHO =======================================
+ECHO =       Bypass Internet Positif       =
+ECHO =======================================
+ECHO Status     = Patched
+ECHO Version    = 1.1
+ECHO.
+ECHO Supported Website
+ECHO 1) KumpulBagi
+ECHO 2) Reddit
+ECHO 3) Vimeo
+ECHO 4) YTS
+ECHO 5) Openload
+ECHO.
+ECHO Check update? Visit files.jealno.com
+PAUSE
+
+
+
+
